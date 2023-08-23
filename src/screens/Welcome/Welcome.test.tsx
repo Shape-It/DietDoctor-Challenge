c5b2ced4e7fb960e321ea58877846b551d43f395
Fede Martinez <federico.martinez.ctr@one.app>
2023-08-23 17:16:24 -0300

    Add tests

--- src/screens/Welcome/Welcome.test.tsx
+++ src/screens/Welcome/Welcome.test.tsx
@@ -3,6 +3,21 @@ import { render, fireEvent } from '@testing-library/react-native';
 import { Welcome } from './Welcome';
 import { strings } from '@/localization';
 
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+
+// Mocking the navigation functions
+const mockNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: mockNavigate,
+  }),
+}));
+
 jest.mock('react-native-linear-gradient', () => 'LinearGradient');
 
 describe('Welcome component', () => {
@@ -17,24 +32,18 @@ describe('Welcome component', () => {
   });
 
   it('calls handleLogin when Login button is pressed', () => {
-    const { getByTestId } = render(<Welcome />);
-    const loginButton = getByTestId(strings.welcome.login);
-
-    const mockHandleLogin = jest.fn();
-    Welcome.prototype.handleLogin = mockHandleLogin;
+    const { getByText } = render(<Welcome />);
+    const loginButton = getByText(strings.welcome.login);
 
     fireEvent.press(loginButton);
-    expect(mockHandleLogin).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalled();
   });
 
   it('calls handleSkip when Skip button is pressed', () => {
-    const { getByTestId } = render(<Welcome />);
-    const skipButton = getByTestId(strings.welcome.skip);
-
-    const mockHandleSkip = jest.fn();
-    Welcome.prototype.handleSkip = mockHandleSkip;
+    const { getByText } = render(<Welcome />);
+    const skipButton = getByText(strings.welcome.skip);
 
     fireEvent.press(skipButton);
-    expect(mockHandleSkip).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalled();
   });
 });
